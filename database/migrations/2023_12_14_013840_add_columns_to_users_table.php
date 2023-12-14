369ea31c5cff971652e4423047ca7e232ca3da7c
Guilherme <guiduartesbs@hotmail.com>
2023-12-13 22:58:54 -0300

    feat: add alter table users

---
+++ database/migrations/2023_12_14_013840_add_columns_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('plan_id');
+            $table->date('date_birth');
+            $table->string('cpf',14)->unique();
+            $table->foreign('plan_id')->references('id')->on('plans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
