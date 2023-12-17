3ed1ddddba92ea4449fa57ab1b6407be5da1793c
Guilherme <guiduartesbs@hotmail.com>
2023-12-16 21:15:35 -0300

    feat: add rota get Exercises

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,8 @@ Route::middleware('auth:sanctum')->group(function () {
     // rotas privadas
     Route::get('dashboard', [DashboardController::class, 'index']);
     Route::post('exercises', [ExerciseController::class, 'store']);
+    Route::get('exercises', [ExerciseController::class, 'index']);
+
 
 
 });
