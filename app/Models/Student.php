7ab5a3fb81bd7396f27a8a8aeccd0f09d4cbe6a3
Guilherme <guiduartesbs@hotmail.com>
2023-12-17 21:49:10 -0300

    feat: adiciona Softdelete no model de student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
+    use SoftDeletes;
     use HasFactory;
+
     protected $fillable = [
         'name',
         'email',
