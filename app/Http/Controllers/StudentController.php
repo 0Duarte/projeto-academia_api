8ae965cf3fe260e6d93a6481f4cc08b690c06a85
Guilherme <guiduartesbs@hotmail.com>
2023-12-20 20:37:52 -0300

    feat: adiciona função update no student controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -79,4 +79,37 @@ class StudentController extends Controller
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
+    public function update($id, Request $request)
+    {
+        try {
+            $data = $request->only('name', 'email', 'date_birth', 'cpf', 'contact', 'city', 'neighborhood', 'number', 'street', 'state', 'complement', 'contact');
+            $request->validate([
+                "name" => "string|max:255",
+                "email" => "string|email|max:255|unique:students",
+                "date_birth" => 'string|date_format:Y-m-d|',
+                "cpf" => "string|max:14|unique:students",
+                "cep" => "string",
+                "street" => "string",
+                "neighborhood" => "string",
+                "city" => "string",
+                "state" => "string",
+                "complement" => "string",
+                "number" => "string",
+                "contact" => "string|max:20|"
+            ]);
+            $student = Student::find($id);
+
+            if (!$student) return $this->error('Estudante não encontrado', Response::HTTP_NOT_FOUND);
+
+            if ($student->user_id != $request->user()->id) {
+                return $this->error('Este estudante pertence a outro usuário', Response::HTTP_FORBIDDEN);
+            }
+
+            $student->update($data);
+
+            return $student;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
