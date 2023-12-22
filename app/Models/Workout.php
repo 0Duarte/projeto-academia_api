991bd144e2ff1a9f87df6f313ce4d20785eb74a8
Guilherme <guiduartesbs@hotmail.com>
2023-12-22 20:51:24 -0300

    feat: cria relacionamento entre tabela exercise e workout

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -9,5 +9,11 @@ class Workout extends Model
 {
     use HasFactory;
 
+    protected $hidden = ['created_at', 'deleted_at', 'updated_at'];
     protected $fillable = ['student_id', 'exercise_id','repetitions','weight','break_time','day','observations','time'];
+
+    public function exercise()
+    {
+        return $this->belongsTo(Exercise::class, 'exercise_id');
+    }
 }
